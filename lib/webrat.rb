de22cf7bf87f712b5f33557a00720d9185826f34
Brian Takita <brian@honk.com>
2010-06-30 19:07:23 -0700

    Fixed conflict.

--- lib/webrat.rb
+++ lib/webrat.rb
@@ -2,11 +2,7 @@ require "rack"
 require "nokogiri"
 
 module Webrat
-<<<<<<< HEAD
-  VERSION = "0.6.0.10"
-=======
   VERSION = "0.7.2.pre"
->>>>>>> 4881ce82bb30528957c9ce9a12fcaf376d7a428a
 
   autoload :MechanizeAdapter, "webrat/adapters/mechanize"
   autoload :MerbAdapter, "webrat/adapters/merb"
